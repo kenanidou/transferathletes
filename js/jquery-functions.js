cd2edb4c6effce0af5782804dffcc1bf75ae9bd6
kenanidou <iis23162@uom.edu.gr>
2025-11-02 22:50:02 +0200

    Update jquery-functions.js

--- js/jquery-functions.js
+++ js/jquery-functions.js
@@ -1,11 +1,14 @@
-$("document").ready(function () {
+$(document).ready(function () {
   var currentQuestion = 0;
   var totalQuestions = 0;
   var userAnswers = {};
   var all_questions;
+  var all_questions_en;
   var all_evidences;
+  var all_evidences_en;
   var faq;
-  var currentLanguage = "greek"; // Αρχική γλώσσα
+  var faq_en;
+  var currentLanguage = "greek";
 
   function hideFormBtns() {
     $("#nextQuestion").hide();
@@ -14,184 +17,142 @@ $("document").ready(function () {
 
   function getQuestions() {
     return fetch("question-utils/all-questions.json")
-      .then((response) => response.json())
+      .then((res) => res.json())
       .then((data) => {
         all_questions = data;
         totalQuestions = data.length;
+        return fetch("question-utils/all-questions-en.json");
       })
-      .catch((error) => {
-        console.error("Failed to fetch all-questions:", error);
-        $(".question-container").html("Error: Failed to fetch all-questions.json.");
+      .then((res) => res.json())
+      .then((dataEn) => {
+        all_questions_en = dataEn;
+      })
+      .catch((err) => {
+        console.error(err);
+        $(".question-container").html("Failed to fetch questions.");
         hideFormBtns();
       });
   }
 
   function getEvidences() {
     return fetch("question-utils/cpsv.json")
-      .then((response) => response.json())
+      .then((res) => res.json())
       .then((data) => {
         all_evidences = data;
+        return fetch("question-utils/cpsv-en.json");
       })
-      .catch((error) => {
-        console.error("Failed to fetch cpsv:", error);
-        $(".question-container").html("Error: Failed to fetch cpsv.json.");
+      .then((res) => res.json())
+      .then((dataEn) => {
+        all_evidences_en = dataEn;
+      })
+      .catch((err) => {
+        console.error(err);
+        $(".question-container").html("Failed to fetch evidences.");
         hideFormBtns();
       });
   }
 
   function getFaq() {
     return fetch("question-utils/faq.json")
-      .then((response) => response.json())
+      .then((res) => res.json())
       .then((data) => {
         faq = data;
+        return fetch("question-utils/faq-en.json");
       })
-      .catch((error) => {
-        console.error("Failed to fetch faq:", error);
-        $(".question-container").html("Error: Failed to fetch faq.json.");
+      .then((res) => res.json())
+      .then((dataEn) => {
+        faq_en = dataEn;
+      })
+      .catch((err) => {
+        console.error(err);
       });
   }
 
   function getEvidencesById(id) {
-    var selectedEvidence = all_evidences.PublicService.evidence.find(
-      (evidence) => evidence.id === id
-    );
-
+    var selectedEvidence = currentLanguage === "greek" ? all_evidences : all_evidences_en;
+    selectedEvidence = selectedEvidence.PublicService.evidence.find((e) => e.id === id);
     if (selectedEvidence) {
-      const evidenceListElement = document.getElementById("evidences");
-      selectedEvidence.evs.forEach((evsItem) => {
-        const listItem = document.createElement("li");
-        listItem.textContent = evsItem.name;
-        evidenceListElement.appendChild(listItem);
+      const el = document.getElementById("evidences");
+      selectedEvidence.evs.forEach((ev) => {
+        const li = document.createElement("li");
+        li.textContent = ev.name;
+        el.appendChild(li);
       });
-    } else {
-      console.log(`Evidence with ID '${id}' not found.`);
     }
   }
 
-  function loadQuestion(questionId, noError) {
+  function loadQuestion(qId, noError) {
     $("#nextQuestion").show();
     if (currentQuestion > 0) $("#backButton").show();
-
-    var question = all_questions[questionId];
-    var questionElement = document.createElement("div");
-
+    var question = currentLanguage === "greek" ? all_questions[qId] : all_questions_en[qId];
+    var qElem = document.createElement("div");
     if (noError) {
-      questionElement.innerHTML = `
-        <div class='govgr-field'>
-          <fieldset class='govgr-fieldset'>
-            <legend class='govgr-fieldset__legend govgr-heading-l'>
-              ${question.question}
-            </legend>
-            <div class='govgr-radios' id='radios-${questionId}'>
-              ${question.options
-                .map(
-                  (option) => `
-                    <div class='govgr-radios__item'>
-                      <label class='govgr-label govgr-radios__label'>
-                        ${option}
-                        <input class='govgr-radios__input' type='radio' name='question-option' value='${option}' />
-                      </label>
-                    </div>
-                  `
-                )
-                .join("")}
-            </div>
-          </fieldset>
-        </div>`;
+      qElem.innerHTML = `<div class='govgr-field'><fieldset class='govgr-fieldset'><legend class='govgr-fieldset__legend govgr-heading-l'>${question.question}</legend><div class='govgr-radios' id='radios-${qId}'>${question.options.map(opt => `<div class='govgr-radios__item'><label class='govgr-label govgr-radios__label'>${opt}<input class='govgr-radios__input' type='radio' name='question-option' value='${opt}' /></label></div>`).join('')}</div></fieldset></div>`;
     } else {
-      questionElement.innerHTML = `
-        <div class='govgr-field govgr-field__error'>
-          <legend class='govgr-fieldset__legend govgr-heading-l'>${question.question}</legend>
-          <p class='govgr-error-message'>Πρέπει να επιλέξετε μια απάντηση</p>
-          <div class='govgr-radios' id='radios-${questionId}'>
-            ${question.options
-              .map(
-                (option) => `
-                <div class='govgr-radios__item'>
-                  <label class='govgr-label govgr-radios__label'>
-                    ${option}
-                    <input class='govgr-radios__input' type='radio' name='question-option' value='${option}' />
-                  </label>
-                </div>
-              `
-              )
-              .join("")}
-          </div>
-        </div>`;
+      qElem.innerHTML = `<div class='govgr-field govgr-field__error'><legend class='govgr-fieldset__legend govgr-heading-l'>${question.question}</legend><p class='govgr-error-message'>Πρέπει να επιλέξετε μια απάντηση</p><div class='govgr-radios' id='radios-${qId}'>${question.options.map(opt => `<div class='govgr-radios__item'><label class='govgr-label govgr-radios__label'>${opt}<input class='govgr-radios__input' type='radio' name='question-option' value='${opt}' /></label></div>`).join('')}</div></div>`;
     }
-
-    $(".question-container").html(questionElement);
+    $(".question-container").html(qElem);
   }
 
-  function retrieveAnswers() {
-    getEvidencesById(1);
-    getEvidencesById(2);
-    getEvidencesById(3);
-    getEvidencesById(4);
+  function determineResult(answers) {
+    var positive = answers[3] === "Ναι" && answers[4] === "Όχι";
+    var negative = answers[4] === "Ναι" && answers[3] === "Όχι";
+    var sendMinistry = answers[2] === "Ναι";
+
+    if (positive && !negative && !sendMinistry) return "positive";
+    else if (!positive && negative && !sendMinistry) return "negative";
+    else if (sendMinistry) return "ministry";
+    else return "mixed";
   }
 
   function submitForm() {
     const resultWrapper = document.createElement("div");
     resultWrapper.setAttribute("id", "resultWrapper");
+    const evidenceList = document.createElement("ol");
+    evidenceList.setAttribute("id", "evidences");
 
-    // Βήματα για λογική εισήγησης
-    const positiveSteps = [4, 6, 8, 10];
-    const negativeSteps = [5, 7, 9, 11];
-    const ministryStep = 3;
-
-    let allPositiveYes = positiveSteps.every(i => userAnswers[i] === "Ναι");
-    let allPositiveNo = positiveSteps.every(i => userAnswers[i] === "Όχι");
-
-    let allNegativeYes = negativeSteps.every(i => userAnswers[i] === "Ναι");
-    let allNegativeNo = negativeSteps.every(i => userAnswers[i] === "Όχι");
-
-    let ministryYes = userAnswers[ministryStep] === "Ναι";
-
-    let finalResult = "";
-
-    if (ministryYes) {
-        finalResult = "Αποστολή αιτήματος προς Υπουργείο Παιδείας: Η περίπτωση χρειάζεται έγκριση/εισήγηση από το Υπουργείο.";
-    } else if (allPositiveYes && allNegativeNo) {
-        finalResult = "Θετική εισήγηση μεταγραφής: Πληροί όλες τις προϋποθέσεις. Προχωρά στη δημιουργία θετικού εγγράφου και πρωτοκόλληση.";
-    } else if (allNegativeYes && allPositiveNo) {
-        finalResult = " Αρνητική εισήγηση μεταγραφής: Λείπουν δικαιολογητικά ή δεν καλύπτονται οι προϋποθέσεις. Προχωρά στη δημιουργία αρνητικού εγγράφου.";
-    } else {
-        finalResult = " Μικτή ή μη αποφασισμένη περίπτωση: Δεν μπορεί να εξαχθεί αποτέλεσμα.";
+    var answers = [];
+    for (var i = 0; i < totalQuestions; i++) {
+      answers.push(sessionStorage.getItem("answer_" + i));
     }
 
-    resultWrapper.innerHTML = `<h1 class='answer'>Η διαδικασία ολοκληρώθηκε!</h1>
-                               <h4 class='answer'>${finalResult}</h4>`;
+    var resultType = determineResult(answers);
+    var titleText = "";
+    if (currentLanguage === "greek") {
+      if (resultType === "positive") titleText = "Θετική εισήγηση μεταγραφής";
+      else if (resultType === "negative") titleText = "Αρνητική εισήγηση μεταγραφής";
+      else if (resultType === "ministry") titleText = "Αποστολή αιτήματος προς Υπουργείο Παιδείας";
+      else titleText = "Δεν υπάρχει σαφές αποτέλεσμα";
+    } else {
+      if (resultType === "positive") titleText = "Positive recommendation";
+      else if (resultType === "negative") titleText = "Negative recommendation";
+      else if (resultType === "ministry") titleText = "Request to Ministry of Education";
+      else titleText = "No clear outcome";
+    }
 
+    resultWrapper.innerHTML = `<h1 class='answer'>${titleText}</h1>`;
     $(".question-container").html(resultWrapper);
 
-    // Εμφάνιση δικαιολογητικών
-    const evidenceListElement = document.createElement("ol");
-    evidenceListElement.setAttribute("id", "evidences");
-    $(".question-container").append(
-      "<br /><h5 class='answer'>Τα δικαιολογητικά που πρέπει να προσκομίσετε είναι:</h5><br />"
-    );
-    $(".question-container").append(evidenceListElement);
+    $(".question-container").append(evidenceList);
+    if (resultType === "positive") getEvidencesById(9); // θετικά δικαιολογητικά
+    else if (resultType === "negative") getEvidencesById(10); // αρνητικά δικαιολογητικά
+    else if (resultType === "ministry") getEvidencesById(3); // προς Υπουργείο
 
-    retrieveAnswers();
     hideFormBtns();
   }
 
   $("#nextQuestion").click(function () {
     if ($(".govgr-radios__input").is(":checked")) {
-      var selectedOption = $('input[name="question-option"]:checked').val();
-      userAnswers[currentQuestion] = selectedOption;
-      sessionStorage.setItem("answer_" + currentQuestion, selectedOption);
-
-      if (currentQuestion + 1 === totalQuestions) {
-        submitForm();
-      } else {
+      var selected = $('input[name="question-option"]:checked').val();
+      userAnswers[currentQuestion] = selected;
+      sessionStorage.setItem("answer_" + currentQuestion, selected);
+      if (currentQuestion + 1 === totalQuestions) submitForm();
+      else {
         currentQuestion++;
         loadQuestion(currentQuestion, true);
-        if (currentQuestion + 1 === totalQuestions) $("#nextQuestion").text("Υποβολή");
+        if (currentQuestion + 1 === totalQuestions) $(this).text("Υποβολή");
       }
-    } else {
-      loadQuestion(currentQuestion, false);
-    }
+    } else loadQuestion(currentQuestion, false);
   });
 
   $("#backButton").click(function () {
@@ -216,11 +177,5 @@ $("document").ready(function () {
 
   $("#questions-btns").hide();
 
-  getQuestions().then(() => {
-    getEvidences().then(() => {
-      getFaq().then(() => {
-        loadQuestion(currentQuestion, true);
-      });
-    });
-  });
+  getQuestions().then(() => getEvidences().then(() => getFaq().then(() => loadQuestion(currentQuestion, true))));
 });
